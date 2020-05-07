c874dbd2bdac257dc2152e5b2d43e637507ee612
jefynhu11 <jefyx1@hotmail.com>
2020-05-07 12:22:04 -0300

    Criei classe AuthenticationAppObject

--- src/test/java/appObjects/AuthenticationAppObject.java
+++ src/test/java/appObjects/AuthenticationAppObject.java
@@ -1,4 +1,17 @@
 package appObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class AuthenticationAppObject {
+    private WebDriver driver;
+
+    public AuthenticationAppObject(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public WebElement getEmailTextField(){
+        return driver.findElement(By.id("email"));
+    }
 }
