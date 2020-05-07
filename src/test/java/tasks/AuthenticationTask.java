d14c369d6f0ca6d46293b8d7de87ac5fe4c43b91
jefynhu11 <jefyx1@hotmail.com>
2020-05-07 17:43:56 -0300

    Adicionei validação e report

--- src/test/java/tasks/AuthenticationTask.java
+++ src/test/java/tasks/AuthenticationTask.java
@@ -1,7 +1,10 @@
 package tasks;
 
 import appObjects.AuthenticationAppObject;
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.WebDriver;
+
+import static frameworks.ReportFw.log;
 
 public class AuthenticationTask {
     private WebDriver driver;
@@ -13,6 +16,11 @@ public class AuthenticationTask {
     }
 
     public void account(String email, String senha){
+        if(driver.getTitle().equals("Login - My Store")) {
+            log(Status.PASS, "Validação - O titulo Login correto.");
+        } else {
+            log(Status.ERROR, "Validação - O titulo Login incorreto");
+        }
         authenticationAppObject.getEmailTextField().sendKeys(email);
         authenticationAppObject.getPasswordTextField().sendKeys(senha);
         authenticationAppObject.getSignInButton().click();
