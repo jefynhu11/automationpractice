16dc25cfbbf4646d3f8e9e7957239006c419df47
jefynhu11 <jefyx1@hotmail.com>
2020-05-07 12:22:55 -0300

    Criei classe AuthenticationTask

--- src/test/java/tasks/AuthenticationTask.java
+++ src/test/java/tasks/AuthenticationTask.java
@@ -1,4 +1,18 @@
 package tasks;
 
+import appObjects.AuthenticationAppObject;
+import org.openqa.selenium.WebDriver;
+
 public class AuthenticationTask {
+    private WebDriver driver;
+    private AuthenticationAppObject authenticationAppObject;
+
+    public AuthenticationTask(WebDriver driver){
+        this.driver = driver;
+        this.authenticationAppObject = new AuthenticationAppObject(driver);
+    }
+
+    public void account(){
+        authenticationAppObject.getEmailTextField().sendKeys("testes@testes.com");
+    }
 }
