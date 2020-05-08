f09ffd31248a93bc23050f7cd58f53807681953c
jefynhu11 <jefyx1@hotmail.com>
2020-05-08 17:19:36 -0300

    Criei classe IndexAppOBject e adicionei elemento clica um produto

--- src/test/java/appObjects/IndexAppObject.java
+++ src/test/java/appObjects/IndexAppObject.java
@@ -1,4 +1,17 @@
 package appObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class IndexAppObject {
+    private WebDriver driver;
+
+    public IndexAppObject(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public WebElement getProductBlouseButton(){
+        return driver.findElement(By.cssSelector("#homefeatured img[title='Blouse']"));
+    }
 }
