da71a8162b12155950748fc616aa09b06c1f50ee
jefynhu11 <jefyx1@hotmail.com>
2020-05-08 17:21:09 -0300

    Criei classe IndexTask, adicionei ação clicar e report

--- src/test/java/tasks/IndexTask.java
+++ src/test/java/tasks/IndexTask.java
@@ -1,4 +1,28 @@
 package tasks;
 
+import appObjects.IndexAppObject;
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.WebDriver;
+
+import static frameworks.ReportFw.log;
+import static frameworks.ScreenshotFw.screenshotCapture;
+
 public class IndexTask {
+    private WebDriver driver;
+    private IndexAppObject indexAppObject;
+
+    public IndexTask(WebDriver driver){
+        this.driver = driver;
+        this.indexAppObject = new IndexAppObject(driver);
+    }
+
+    public void selectProduct(){
+        if(driver.getTitle().equals("My Store")){
+            log(Status.PASS, "Validação - O titulo My Store correto.");
+            indexAppObject.getProductBlouseButton().click();
+        }else {
+            log(Status.ERROR, "Validação - O titulo My Store incorreto", screenshotCapture(driver));
+        }
+
+    }
 }
