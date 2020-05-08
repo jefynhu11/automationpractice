b3d7d6f2fd81f1f8a3d72a76eed2e66736067acb
jefynhu11 <jefyx1@hotmail.com>
2020-05-08 17:22:23 -0300

    Criei classe MyAccountAppObject e adicionei elemento 'Home'

--- src/test/java/appObjects/MyAccountAppObject.java
+++ src/test/java/appObjects/MyAccountAppObject.java
@@ -1,4 +1,17 @@
 package appObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class MyAccountAppObject {
+    private WebDriver driver;
+
+    public MyAccountAppObject(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public WebElement getHomeButton(){
+        return driver.findElement(By.cssSelector("a[title='Home']"));
+    }
 }
